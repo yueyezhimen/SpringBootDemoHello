038adfd2ee9d028cbf5407528093c3d515fa4c54
yueye <yueyezhimen@qq.com>
2024-04-30 00:22:39 +0800

    1

--- src/main/java/com/springboot/hello/HelloController.java
+++ src/main/java/com/springboot/hello/HelloController.java
@@ -3,6 +3,7 @@ package com.springboot.hello;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,7 +15,7 @@ import jakarta.servlet.http.HttpServletRequest;
 @RequestMapping(value ="hello")
 @Controller
 public class HelloController {
-    @RequestMapping(value ="helloworld")
+    @GetMapping(value ="helloworld")
     @ResponseBody
     public Map<String, Object> updateSampleById(User user, HttpServletRequest request) {
         return ResultUtils.setResult(true, 1, "HelloWorld");
